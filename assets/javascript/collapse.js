d10329952d1c80227140208b4e5f6d5027847a8b
Arkay <riyadh.k@outlook.com>
2021-09-28 15:59:17 +0000

    Combine js into single file

--- assets/javascript/collapse.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.accordion__button').forEach(button => {
-    button.addEventListener('click', () => {
-        button.classList.toggle('accordion__button--active');
-        
-    });
-});
-
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,18 @@
+/* jshint esversion:8 */
+
+// Burger Menu
 const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('nav-ul');
 
 hamburger.addEventListener('click', () => {
     navUL.classList.toggle('show');
 
-})
+});
 
+// Collapsible Buttons
+document.querySelectorAll('.accordion__button').forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.toggle('accordion__button--active');
+        
+    });
+});
